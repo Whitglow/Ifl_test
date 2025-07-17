62eb35ae68f6b3943968e4de389924d591c1db26
Konstantin <133163011+Whitglow@users.noreply.github.com>
2025-07-17 20:34:40 +0300

    Update Task_1.py
    Update_17_07

--- Task_1.py
+++ Task_1.py
@@ -2,20 +2,14 @@ import random
 
 # Создаем массив из 10 случайных чисел от 0 до 1
 array_size = 10
-array = [random.randint(0, 10) for _ in range(array_size)]
+array = [random.random() for _ in range(array_size)]
 
 # Вычисляем максимальное, минимальное и среднее значение
 max_value = max(array)
 min_value = min(array)
 avg_value = sum(array) / len(array)
 
-# Вычисляем обычное среднее значение
-avg_value = sum(array) / len(array)
-
-# Находим ближайшее к среднему число
-closest_to_avg = min(array, key=lambda x: abs(x - avg_value))
-
 print(f"Массив: {array}")
 print(f"Максимальное: {max_value}")
 print(f"Минимальное: {min_value}")
-print(f"Среднее: {closest_to_avg}")
+print(f"Среднее: {avg_value}")
